31c903c14ffaa7a3843d08302a019e70502f88e6
shubham75566 <sssoni766@gmail.com>
2023-11-01 11:48:20 +0530

    first commit

---
+++ src/test/java/com/example/kd/KdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
